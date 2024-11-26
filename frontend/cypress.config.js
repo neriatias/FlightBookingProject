c1dcd60c2cd47bf5d226fc733e7c0e051c30fb8a
neriatias <neria126@gmail.com>
2024-11-26 14:21:18 +0200

    try to fix

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000", // כתובת האתר שלך
-    defaultCommandTimeout: 40000, 
     setupNodeEvents(on, config) {
       // הוסף כאן כל אירועים מותאמים אישית אם יש
     },
