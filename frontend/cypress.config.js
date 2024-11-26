8f42025d6e12c6179da6e88927da036724481b28
neriatias <neria126@gmail.com>
2024-11-26 11:37:25 +0200

    Wait for local host

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000", // כתובת האתר שלך
+    defaultCommandTimeout: 40000, 
     setupNodeEvents(on, config) {
       // הוסף כאן כל אירועים מותאמים אישית אם יש
     },
