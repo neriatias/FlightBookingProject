7f7e6bd9cdd1df19328cf52219f7b9b630feab7c
neriatias <neria126@gmail.com>
2025-03-02 14:24:37 +0200

    Update backend URL for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5002', // הכתובת של ה-Backend שלך
+  baseURL: 'https://flight-backend-efms.onrender.com', // הכתובת של ה-Backend שלך
 });
 
 export default api;
