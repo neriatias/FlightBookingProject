139bb1b7cf83ebba2dca89a0d870aff0b87f4523
neriatias <neria126@gmail.com>
2025-03-04 11:56:10 +0200

    fix the base url for cypress

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000", // כתובת האתר שלך
+    baseUrl: "https://flight-frontend-q55m.onrender.com", // כתובת האתר שלך
     setupNodeEvents(on, config) {
       // הוסף כאן כל אירועים מותאמים אישית אם יש
     },
