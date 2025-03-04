4c98edbe56269ebc126717330dcb83471973bbbb
neriatias <neria126@gmail.com>
2025-03-04 11:35:29 +0200

    Updated database.js to use DATABASE_URL from Render

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME || 'flightdb',
-  process.env.DATABASE_USER || 'admin',
-  process.env.DATABASE_PASSWORD || 'secret',
-  {
-    host: process.env.DATABASE_HOST || 'localhost',
-    dialect: 'postgres',
-    port: process.env.DATABASE_PORT || 5433,
-    logging: false,
-    retry: {
-      max: 10, // נסה להתחבר 10 פעמים לפני כשל
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false, // הימנע מבעיות תעודות
     },
-  }
-);
+  },
+  logging: false, // ביטול לוגים של Sequelize
+  retry: {
+    max: 10, // נסה להתחבר 10 פעמים לפני כשל
+  },
+});
 
 module.exports = sequelize;
